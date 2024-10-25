b107442d2dd57e6d38fea78b5b9aedaff2a0ac96
William Wong <williamw55955@gmail.com>
2024-10-25 15:45:06 -0700

    actaully linking the database to UI

--- README.md
+++ README.md
@@ -3,15 +3,15 @@
  Make it deploy (vercel) x
  Scaffold basic ui with mock data x
  Tidy up build process x
- Actually set up a database (vercel postgres)
- Attach database to UI
+ Actually set up a database (vercel postgres) x
+ Attach database to UI x
  Add authentication (w/ clerk)
  Add image upload
- "taint" (server-only)
+ "taint" (server-only)W
  Use Next/Image component
  Error management (w/ Sentry)
  Routing/image page (parallel route)
- Update upload button to be less cringe
+ Update upload button Sto be less cringe
  Analytics (posthog)
  Delete button (w/ Server Actions)
  Ratelimiting (upstash)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,27 @@
 import { db } from "~/server/db";
-import { posts } from "~/server/db/schema";
+import { images } from "~/server/db/schema";
+import { desc } from "drizzle-orm/expressions";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/EtzqxYn2kxdSXwdBP9C380LVnCeSM19FrfYduBwGgIKckDtR",
-  "https://utfs.io/f/EtzqxYn2kxdSNhhFJDc4WpSwz2gKU58EsR3HZGjmDNMvQAd6",
-  "https://utfs.io/f/EtzqxYn2kxdSggouHGEtpo34NIvgwHAQXlnd8ZiFrfs95Kyx",
-  "https://utfs.io/f/EtzqxYn2kxdSOC00GYimVaqTsScEvz7FkAM5UPNJyuGo0xfw"
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const postsData = await db.select().from(posts);
-
-  console.log(postsData);
+  const imagesData = await db
+    .select()
+    .from(images)
+    .orderBy(desc(images.id));
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {postsData.map((post) => (
-          <div key = {post.id}>{post.name}</div>
-        )) }
-        {[...mockImages, ...mockImages, ...mockImages, ...mockImages, ...mockImages,].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+        {/* Repeating imagesData multiple times to show the sequence */}
+        {[...imagesData, ...imagesData, ...imagesData, ...imagesData, ...imagesData].map((image, index) => (
+          <div key={`${image.id}-${index}`} className="w-48 flex flex-col">
             <img src={image.url} alt={`image-${image.id}`} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
-      <p>Website in progress</p> {/* Wrap "website in progress" inside a tag */}
+      <p>Website in progress</p>
     </main>
   );
 }
