673855ed5d4a3600d29fc0661273c2fb9eb664e0
William Wong <williamw55955@gmail.com>
2024-10-30 21:26:09 -0700

    deleted unnecessary schemas alongside their queries

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,25 +1,9 @@
 import "server-only";
 import { db } from "~/server/db";
-import { images } from "~/server/db/schema";
 import { desc } from "drizzle-orm/expressions";
 import { courses} from "~/server/db/schema" // add roadmap later
-import { type Course } from "~/server/db/types"
-// import { eq } from "drizzle-orm/expressions";
-// Define the return type based on your schema
-export async function getImages() {
-    const imagesData = await db
-      .select()
-      .from(images)
-      .orderBy(desc(images.id));
-  
-    return imagesData; // Make sure to return the fetched data
-  }
-// export async function getCoursesForMajor(majorId: string) {
-//   const coursesData = await db.select().from(courses)  // Ensure "courses" is defined in schema.ts
-//   .where(eq(courses.majorId, majorId))
-//   .orderBy(courses.sequence); // Assuming sequence in the roadmap
-//   return coursesData;
-// }
+import { type Course } from "~/server/db/types" 
+
 export async function getCourses(): Promise<Course[]>{
   const courseData = await db
       .select({id: courses.id,
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -19,24 +19,6 @@ import {
  */
 export const createTable = pgTableCreator((name) => `t3willdevtop_${name}`);
 
-export const images = createTable(
-  "image",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }).notNull(),
-    url: varchar("url", { length: 1024 }).notNull(),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
-
 export const courses = createTable(
   "courses", {
   id: serial("id").primaryKey(),
